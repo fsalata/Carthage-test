0c4a8161fcc0704561a795842ac29548eb89f029
Fábio Salata <s2it_fsalata@uolinc.com>
2020-02-26 15:36:13 -0300

    Configuration

---
+++ Carthage/Build/iOS/Carthage_test.framework/Headers/Carthage_test.h
@@ -0,0 +1,19 @@
+//
+//  Carthage_test.h
+//  Carthage-test
+//
+//  Created by Fábio Salata on 26/02/20.
+//  Copyright © 2020 PagSeguro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Carthage_test.
+FOUNDATION_EXPORT double Carthage_testVersionNumber;
+
+//! Project version string for Carthage_test.
+FOUNDATION_EXPORT const unsigned char Carthage_testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Carthage_test/PublicHeader.h>
+
+
---
+++ Test.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  Carthage-test
+//
+//  Created by Fábio Salata on 26/02/20.
+//  Copyright © 2020 PagSeguro. All rights reserved.
+//
+
+import Foundation
+
+public struct Greetings {
+    static public func sayHello() {
+        print("hello")
+    }
+}
